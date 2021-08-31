051cbd0aa1aab2ea69ec9fb5a45bcead9d1d41fd
CryptoPentest <88625998+CryptoPentest@users.noreply.github.com>
2021-08-31 12:00:14 +0300

    Add files via upload

---
+++ Poker_Random_Generator.py
@@ -0,0 +1,38 @@
+import random
+
+
+def random_number(l1,l2):
+    
+    return random.randint(l1,l2)
+
+from tkinter import*
+
+root = Tk()
+root.geometry("250x100")
+root.configure(bg="black")
+root.title("Poker_Random_Generator")
+root.grid()
+
+
+
+def std_poker():
+    value =random.randint(1,100)
+    print(value)
+    if (value<=35):
+        number.config(text=value,foreground="red")
+    elif (value >=75):
+        number.config(text=value,foreground="black")
+    else:
+        number.config(text=value,foreground="blue")
+
+
+button_1= Button( text="Generate[1-100]",bg="white",command=std_poker)
+button_1.pack()
+button_1.place(height=35,width=155,x=50,y=10)
+
+
+number=Label(text="",foreground="blue")
+number.pack()
+number.place(height=20,width=60,x=100,y=70)
+
+root.mainloop()
